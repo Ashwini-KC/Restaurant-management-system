fb6630588d3a878c2de57f4612128754668cc32b
Pragya <Pragya@DESKTOP-M5NL4TB>
2022-12-05 00:21:33 +0530

    test

---
+++ src/test_Customer.py
@@ -0,0 +1,32 @@
+    
+def customer_details():
+    '''
+      Test
+    '''
+    pass
+
+
+def ordered_items():
+    '''
+       Test
+    '''
+    pass
+
+def payment_status():
+    '''
+      Test
+    '''
+    pass
+
+def add_customer():
+    '''
+     idk why this is here, but same functionality as add_employee - as per @shibasis
+    '''
+    pass
+    
+def delete_customer():
+    '''
+    test
+        
+    '''
+    pass
---
+++ src/test_Menu.py
@@ -0,0 +1,12 @@
+from Menu import Menu
+
+def test_get_item(self):
+        pass
+    
+def test_add_item(self, item):
+   menu = Menu()
+   menu.add_item("naan")
+   assert "naan"  in menu.get_item()
+       
+   pass  
+    
