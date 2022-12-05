5ee5bd02cb93c34c3607ac2544ccb2075a1b6ee7
Pragya <Pragya@DESKTOP-M5NL4TB>
2022-12-05 16:33:42 +0530

    Testbill

---
+++ src/test_Bill.py
@@ -0,0 +1,11 @@
+ 
+def test_calculate_Bill():
+   
+    '''Test calculate Bill here'''
+   
+    pass
+
+def test_generate_Bill():
+
+    '''Test Generate Bill here'''
+    pass
--- src/test_Menu.py
+++ src/test_Menu.py
@@ -1,12 +1,17 @@
-from Menu import Menu
+from Menu import Menu_
 
-def test_get_item(self):
+def __init__(self):
         pass
     
-def test_add_item(self, item):
-   menu = Menu()
-   menu.add_item("naan")
-   assert "naan"  in menu.get_item()
-       
-   pass  
+def add_item(self, item):
+        menu = Menu_()
+        menu.add_item("naan")
+        assert "naan"  in menu.get_item()
+
+def delete_item(self, id):
+        menu = Menu_()
+        menu.delete_item(2)
+        assert 2 not in menu.get_item()
+
+
     
