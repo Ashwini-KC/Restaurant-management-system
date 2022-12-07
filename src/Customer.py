d8f25dd7d961295a80397d66d6df447718d2ce41
Ashwini-KC <115699658+Ashwini-KC@users.noreply.github.com>
2022-12-07 17:48:00 +0000

    Add files via upload

--- src/Customer.py
+++ src/Customer.py
@@ -4,8 +4,27 @@ class Customer:
     def __init__(self,custId,custName,custType): 
         self.custId=custId
         self.custName=custName
-        self.custType=custType     
+        self.custType=custType    
 
+    def get_custId(self):
+        return self.custId
+    
+    def set_custId(self,id):
+        self.custId = id
+
+    def get_custName(self):
+        return self.custName
+
+    def set_custName(self,name):
+        self.custName = name
+
+    def get_custType(self):
+        return self.custType
+
+    def set_custType(self,type):
+        self.custType = type
+
+    
     def customer_details(self):
         '''
         show customerdetails when custID is passed
